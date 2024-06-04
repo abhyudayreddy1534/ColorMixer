e7569a41f26e62dd7da115fb3d736c2044310db2
Abhyuday Reddy Nandikonda <abhyudayreddy1534@gmail.com>
2024-06-04 10:48:21 -0500

    Initial Commit

---
+++ ColorMixer/ColorMixerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorMixerApp.swift
+//  ColorMixer
+//
+//  Created by Sravanthi Chinthireddy on 04/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ColorMixerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
